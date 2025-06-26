ff2ec6d183c5b38b01d6cdc20de2d057e4c53d76
uche umoga <ucheumoga@gmail.com>
2025-06-26 11:39:26 -0500

    env clean up

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ duve_reservation_details: Dict[str, Dict[str, Any]] = {}
 smoobu_reservations: Dict[str, Dict[str, Any]] = {}
 
 DUVE_HOOK_URL = "https://connect.duve.com/api/v1/hooks/duveconnect?pid=6856e4877bd2d41f36a42210"
-DUVE_BEARER_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImNvbXBhbnkiOiI1ZTJjNmQwNzk0ODQ2MTBjNDcxMTIwYmIiLCJjb25uZWN0aW9uIjoiNjg1NmU0ODc3YmQyZDQxZjM2YTQyMjEwIiwiYWRkaXRpb25hbEZpZWxkcyI6WyJhbXlmaW5laG91c2VfYm9va2luZ19kYXRlX0ZwbmQyelVkM0wiXX0sImlzcyI6IkR1dmUiLCJqdGkiOiJ2NThHa25EaHoifQ.lkLhikJYtO13qcMq-AZF9CnBHhTWx2xl9KcpdtI5fxo"  # Replace with your actual Duve token
+DUVE_BEARER_TOKEN = os.getenv("DUVE_BEARER_TOKEN")  # Replace with your actual Duve token
 
 
 class DuveHookPayload(BaseModel):
@@ -136,15 +136,14 @@ async def receive_duve_webhook(payload: DuveHookPayload):
         return {"error": "reservationId missing from Duve payload"}
     
     duve_reservation_details[external_id] = payload.resource
-    print("--about to forward from DUVE")
     await try_forward_combined_data(external_id)
     return {"status": "Duve data received"}
 
 
 @app.post("/webhook")
 async def receive_webhook(payload: WebhookPayload):
-    print(f"Received {payload.action} from user {payload.user}")
-    print(payload.data)
+    # print(f"Received {payload.action} from user {payload.user}")
+    # print(payload.data)
 
     # You can implement custom logic here:
     # if payload.action == "updateRates":
@@ -153,7 +152,6 @@ async def receive_webhook(payload: WebhookPayload):
         # handle_new_reservation(payload.user, payload.data)
         external_id = str(payload.data.get("id"))
         smoobu_reservations[external_id] = payload.data
-        print("--about to forward from SMOOBU")
         await try_forward_combined_data(external_id)
     # elif payload.action == "cancelReservation":
     #     handle_cancel_reservation(payload.user, payload.data)
@@ -167,9 +165,8 @@ async def try_forward_combined_data(external_id: str):
     duve_data = duve_reservation_details.get(external_id)
     smoobu_data = smoobu_reservations.get(external_id)
 
-    print(f"duve dataaaaa: {duve_data}")
-
-    print(f"smoobu dataaaaa: {smoobu_data}")
+    print(f"duve data: {duve_data}")
+    print(f"smoobu data: {smoobu_data}")
     
     if duve_data and smoobu_data:
         # Both hooks received, extract and send to Duve
@@ -196,9 +193,6 @@ async def try_forward_combined_data(external_id: str):
             "Authorization": f"Bearer {DUVE_BEARER_TOKEN}",
             "Content-Type": "application/json"
         }
-
-        print("*******")
-        print(duve_payload)
 
         async with httpx.AsyncClient() as client:
             response = await client.post(DUVE_HOOK_URL, json=duve_payload, headers=headers)
